7c59a5da759a393f2bab4cdeccb526c262528d02
Ris <r.andriska05@gmail.com>
2024-09-18 17:49:58 +0200

    feat(tisza-auth): fix player not getting kicked if is not linked

--- tiszaauth/src/main/java/hu/ris/tiszaauth/listeners/PlayerJoinListener.java
+++ tiszaauth/src/main/java/hu/ris/tiszaauth/listeners/PlayerJoinListener.java
@@ -3,18 +3,20 @@ package hu.ris.tiszaauth.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 
 import hu.ris.tiszaauth.TiszaAuth;
 import hu.ris.tiszaauth.cache.CachedPlayers;
+import net.kyori.adventure.text.Component;
 
 public class PlayerJoinListener implements Listener {
 
     private static final TiszaAuth plugin = TiszaAuth.getInstance();
 
     @EventHandler
-    public void onPlayerJoin(AsyncPlayerPreLoginEvent event) {
-        String playerName = event.getName();
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getName();
 
         if (CachedPlayers.isPlayerCached(playerName)) {
             return;
@@ -29,8 +31,14 @@ public class PlayerJoinListener implements Listener {
                     return;
                 }
 
-                event.disallow(Result.KICK_OTHER, "§cAmíg nem csatlakoztattad össze a Discord fiókodat a karaktereddel, addig nem tudsz belépni a szerverre!");
-            }
+                plugin.getServer().getScheduler().runTask(plugin, new Runnable() {
+                    @Override
+                    public void run() {
+                        event.getPlayer().kick(Component.text("§cAmíg nem csatlakoztattad össze a Discord fiókodat a karaktereddel, addig nem tudsz belépni a szerverre!"));
+                    }
+                });
+            };
+
         });
 
     }
