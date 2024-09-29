97f2ea30c838a2599c05103971ed55613da562ea
ikoliHU <szilagypet53@gmail.com>
2024-09-29 19:43:56 +0200

    feat(builder): add initial project setup and plugin configuration
    - Added Maven project configuration with dependencies and repositories.
    - Created Java class extending JavaPlugin for plugin functionality.
    - Included basic plugin.yml configuration for the builder plugin.

---
+++ builder/src/main/java/hu/ikoli/Builder.java
@@ -0,0 +1,17 @@
+package hu.ikoli;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Builder extends JavaPlugin
+{
+
+  public void onEnable()
+  {
+    
+  }
+
+  public void onDisable()
+  {
+
+  }
+}
