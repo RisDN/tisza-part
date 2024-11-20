796fcb88328922aa47d89b4be4ac4305dc999ea3
Ris <r.andriska05@gmail.com>
2024-11-20 18:52:20 +0100

    feat(tisza-auto-creative): added plugin

---
+++ TiszaAutoCreative/src/main/java/hu/ris/tiszaautocreative/TiszaAutoCreative.java
@@ -0,0 +1,35 @@
+package hu.ris.tiszaautocreative;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TiszaAutoCreative extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+
+        getLogger().info("TiszaAutoCreative started.");
+
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("TiszaAutoCreative stopped.");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (!event.getPlayer().getGameMode().equals(GameMode.CREATIVE)) {
+            event.getPlayer().setGameMode(GameMode.CREATIVE);
+        }
+    }
+
+}
