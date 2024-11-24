659b86c57ec7647d2bba3342c8fd8824d8839062
ikoliHU <szilagypet53@gmail.com>
2024-11-24 12:23:17 +0100

    feat(tisza-bot): add method to generate offline UUID
    Added a method to generate an offline UUID for unlinking characters, improving ban check functionality.

--- tisza-bot/src/commands/unlink.ts
+++ tisza-bot/src/commands/unlink.ts
@@ -1,7 +1,7 @@
 import { Command } from '@sapphire/framework';
+import crypto from "crypto";
 import { config } from '../config';
 import query from '../database/db';
-import crypto from "crypto";
 import { CooldownService } from '../services/CooldownService';
 
 
@@ -45,15 +45,30 @@ export class UnLinkCommand extends Command {
             return interaction.reply({ content: 'Ez a Discord fiók egy karakterhez sincs kapcsolva!', ephemeral: true });
         }
 
-        const uuid = crypto.createHash('md5').update(`$OfflinePlayer:${links[0].username}`).digest('hex');
+        const uuid = this.generateOfflineUUID(links[0].username);
         const bans = (await query('SELECT * FROM litebans_bans WHERE uuid = ? AND active = 1', [uuid]) as any[]);
+
         if (bans.length > 0) {
-            return interaction.reply({ content: 'Ez a karakter **bannolva van** a szerveren, nem tudod szétkapcsolni!', ephemeral: true });
+            return interaction.reply({ content: 'Ez a karakter **ki van tiltva** a szerveren, ezért nem tudod szétkapcsolni!', ephemeral: true });
         }
 
         cooldown?.addCooldown(member.id, config.commandCooldowns.link);
         await query('DELETE FROM links WHERE discord_id = ?', [member.id]);
         member.roles.remove('1307294590901944370');
         return interaction.reply({ content: '**Sikeresen** szétkapcsoltad fiókjaid!', ephemeral: true });
+    }
+
+    private generateOfflineUUID(username: string): string {
+        const hash = crypto.createHash('md5').update(`OfflinePlayer:${username}`).digest('hex');
+
+        const uuid = [
+            hash.substr(0, 8),
+            hash.substr(8, 4),
+            `3${hash.substr(13, 3)}`, // A 13. karakter 3-as
+            `${(parseInt(hash.substr(16, 2), 16) & 0x3f | 0x80).toString(16)}${hash.substr(18, 2)}`, // A 17. karakter 4-es
+            hash.substr(20, 12)
+        ].join('-');
+
+        return uuid;
     }
 }
