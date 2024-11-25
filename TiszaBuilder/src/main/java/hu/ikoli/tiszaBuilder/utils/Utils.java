22e11e37114083212e489febcdaca2badbb1c1ab
Ris <r.andriska05@gmail.com>
2024-11-25 19:24:47 +0100

    chore(tisza-builder): remove unused methods from utils

--- TiszaBuilder/src/main/java/hu/ikoli/tiszaBuilder/utils/Utils.java
+++ TiszaBuilder/src/main/java/hu/ikoli/tiszaBuilder/utils/Utils.java
@@ -4,24 +4,11 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-
-import hu.ikoli.tiszabuilder.config.Config;
 import net.md_5.bungee.api.ChatColor;
 
 public class Utils {
 
     private static final Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
-
-    public static boolean hasSpace(Player player) {
-        return player.getInventory().firstEmpty() != -1;
-    }
-
-    public static int getFinalAmountAfterFees(int amount) {
-        double feePercent = Config.getDouble("settings.fyrecoin-fee-percent");
-        return (int) Math.round(amount * (1 + (feePercent / 100)));
-    }
 
     public static List<String> color(List<String> messages) {
         for (int i = 0; i < messages.size(); i++) {
@@ -40,54 +27,6 @@ public class Utils {
             }
         }
         return ChatColor.translateAlternateColorCodes('&', message);
-    }
-
-    public static void error(String message) {
-        Bukkit.getLogger().severe(message);
-    }
-
-    public static void warn(String message) {
-        Bukkit.getLogger().warning(message);
-    }
-
-    public static void warn(Integer message) {
-        warn(message.toString());
-    }
-
-    public static void warn(Double message) {
-        warn(message.toString());
-    }
-
-    public static void warn(Float message) {
-        warn(message.toString());
-    }
-
-    public static void warn(Boolean message) {
-        warn(message.toString());
-    }
-
-    public static void log(String message) {
-        Bukkit.getLogger().info(message);
-    }
-
-    public static void log(Long message) {
-        log(message.toString());
-    }
-
-    public static void log(Integer message) {
-        log(message.toString());
-    }
-
-    public static void log(Double message) {
-        log(message.toString());
-    }
-
-    public static void log(Float message) {
-        log(message.toString());
-    }
-
-    public static void log(Boolean message) {
-        log(message.toString());
     }
 
 }
