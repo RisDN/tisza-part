b4a16583af94e62bbdd760a0eb787946f82b7fb3
Ris <r.andriska05@gmail.com>
2024-11-30 10:14:37 +0100

    feat(tisza-builder): added flying to building server

--- TiszaBuilder/src/main/java/hu/ikoli/tiszaBuilder/listeners/PlayerJoinListener.java
+++ TiszaBuilder/src/main/java/hu/ikoli/tiszaBuilder/listeners/PlayerJoinListener.java
@@ -5,12 +5,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import hu.ikoli.tiszabuilder.building.BuildingPlayer;
+import hu.ikoli.tiszabuilder.config.Config;
+import hu.ikoli.tiszabuilder.config.ServerType;
 
 public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         BuildingPlayer.getBuildingPlayer(event.getPlayer());
+
+        if (Config.getServerType().equals(ServerType.BUILDING)) {
+            event.getPlayer().setAllowFlight(true);
+            event.getPlayer().setFlying(true);
+        }
+
     }
 
 }
